28b53c1ecff7a905c34907ffddea8f03e690fbf2
Ronggur Habibun <ronggur.mh@gmail.com>
2026-01-13 17:24:56 +0700

    fix: enable GraphQL introspection
    - Add introspection: true to Apollo Server configuration
    - Fix GraphQL playground unable to introspect endpoint
    - Enable schema exploration and auto-completion in GraphQL playground
    
    Configuration:
    - Apollo Server introspection now enabled
    - Allows GraphQL clients to fetch schema information
    - Required for GraphQL playground and IDE integration

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,9 @@ module.exports = ({ env }) => ({
       playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
+      apolloServer: {
+        introspection: true,
+      },
     },
   },
 });
